da4d7e8030ea24d4eacc46eccd3b2254cbc1d831
camilaf <cdetrincheria@hotmail.com>
2021-07-22 01:59:00 -0300

    Fix name

--- src/test_images_requests.py
+++ src/test_images_requests.py
@@ -4,7 +4,7 @@ import uuid
 BASE = "http://127.0.0.1:5000/api/v0/"
 user_id = uuid.uuid4()
 
-# Create notice
+# POST images
 response1 = requests.post(BASE + "preprocessed-images", data={'petImages': []})
 new_report = response1.json()
 assert response1.status_code == 200
